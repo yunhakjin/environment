0b46888b603b32b9e1181eaec95f8ad10e995721
yunhakjin <yunhakjin@163.com>
2018-08-14 09:05:15 +0800

    项目框架初始化
    Signed-off-by: yunhakjin <yunhakjin@163.com>

---
+++ src/main/java/com/springboot/environment/EnvironmentApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.environment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnvironmentApplication.class, args);
+    }
+}
