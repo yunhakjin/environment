28171e11d702a89fe44509154b57bb9346caa548
yunhakjin <yunhakjin@163.com>
2018-10-11 21:23:30 +0800

    日数据部分基础类完成
    Signed-off-by: yunhakjin <yunhakjin@163.com>

---
+++ src/main/java/com/springboot/environment/controller/DataController.java
@@ -0,0 +1,9 @@
+package com.springboot.environment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("data")
+public class DataController {
+}
