c6893f7cf507ebc05abebf68cb5c0fa31722276c
AdrainYin <ych19941103@163.com>
2018-10-13 17:50:57 +0800

    站点查询服务类的编写

--- src/main/java/com/springboot/environment/bean/Station.java
+++ src/main/java/com/springboot/environment/bean/Station.java
@@ -1,88 +1,129 @@
 package com.springboot.environment.bean;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 /**
  * 站点表实体类
  */
-public class Station {
+@Entity
+@Table(name = "station")
+public class Station implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+
+    /**
+     * 站点的主键id，没有实际的意义
+     */
+    @Id
+    @GeneratedValue
+    @Column(name = "ID", unique = true, nullable = false)
+    private int id;
     /**
      * 对内站点编号
      */
+    @Column(name = "STATION_ID", nullable = false)
     private String stationId;
     /**
      * 对外站点编号
      */
+    @Column(name = "STATION_CODE", nullable = false)
     private String stationCode;
     /**
      * 站点名称
      */
+    @Column(name = "STATION_NAME", nullable = false)
     private String stationName;
     /**
      * 站点状态
      * 0 停运 1 运营
      */
+    @Column(name = "STATION_STATUS", nullable = false)
     private int stationStatus;
     /**
      * 所属应用(暂未使用)
      */
+    @Column(name = "APPLICATION")
     private String application;
     /**
      * 在线标识
      * 0 短线 1 在线
      */
+    @Column(name = "ONLINE_FLAG", nullable = false)
     private int onlineFlag;
     /**
      * 对照码
      */
+    @Column(name = "STATION_ID_DZ", nullable = false)
     private String stationIdDZ;
     /**
      * 所用协议
      * 1 老协议 2 新协议
      */
+    @Column(name = "PROTOCOL", nullable = false)
     private int protocol;
     /**
      * 所用协议名称
      */
+    @Column(name = "PROTOCOL_NAME", nullable = false)
     private String protocolName;
     /**
      * 站点位置
      */
+    @Column(name = "POSITION", nullable = false)
     private String position;
     /**
      * 站点所属街道
      */
+    @Column(name = "STREET", nullable = false)
     private String street;
     /**
      * 站点所属行政区
      */
+    @Column(name = "DISTRICT", nullable = false)
     private String district;
     /**
      * 噪声点范围
      */
+    @Column(name = "RANGE", nullable = false)
     private String range;
     /**
      * 国控
      * 0 否 1 是
      */
+    @Column(name = "COUNTRY_CON", nullable = false)
     private int countryCon;
     /**
      * 市控
      * 0 否 1 是
      */
+    @Column(name = "CITY_CON", nullable = false)
     private int cityCon;
     /**
      * 区控
      * 0 否 1 是
      */
+    @Column(name = "DOMAIN_CON", nullable = false)
     private int domainCon;
     /**
      * 区域环境
      */
+    @Column(name = "AREA", nullable = false)
     private int area;
     /**
      * 功能区
      */
+    @Column(name = "DOMAIN", nullable = false)
     private int domain;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getStationId() {
         return stationId;
@@ -231,7 +272,8 @@ public class Station {
     @Override
     public String toString() {
         return "Station{" +
-                "stationId='" + stationId + '\'' +
+                "id=" + id +
+                ", stationId='" + stationId + '\'' +
                 ", stationCode='" + stationCode + '\'' +
                 ", stationName='" + stationName + '\'' +
                 ", stationStatus=" + stationStatus +
--- src/main/java/com/springboot/environment/dao/StationDao.java
+++ src/main/java/com/springboot/environment/dao/StationDao.java
@@ -1,5 +1,113 @@
 package com.springboot.environment.dao;
 
-public interface StationDao {
+import com.springboot.environment.bean.Station;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Map;
+
+public interface StationDao extends JpaRepository<Station, Integer> {
+
+    /**
+     * 查询所有的站点信息，返回list
+     * @return
+     */
+    List<Station> findAll();
+
+
+    //站点的单维度查询
+
+    /**
+     * 根据站点对内编号查询站点信息
+     * @param stationId
+     * @return
+     */
+    Station findByStationId(int stationId);
+
+    /**
+     * 根据站点对外编号查询站点信息
+     * @param stationCode
+     * @return
+     */
+    Station findByStationCode(String stationCode);
+
+    /**
+     * 根据站点名称查询站点信息
+     * @param stationName
+     * @return
+     */
+    Station findByStationName(String stationName);
+
+    /**
+     * 根据站点状态查询所有站点信息，返回list
+     * @param stationStatus
+     * @return
+     */
+    List<Station> findByStationStatus(int stationStatus);
+
+    /**
+     * 根据站点在线标识查询站点信息，返回list
+     * @param onlineFlag
+     * @return
+     */
+    List<Station> findByOnlineFlag(int onlineFlag);
+
+    /**
+     * 通过行政区查询站点信息，枚举
+     * @param district
+     * @return
+     */
+    List<Station> findByDistrict(String district);
+
+    /**
+     * 根据街道查询站点信息，返回list
+     * @param street
+     * @return
+     */
+    List<Station> findByStreet(String street);
+
+    /**
+     * 根据国控查询站点信息
+     * @param isCountryCon
+     * @return
+     */
+    List<Station> findByCountryCon(int isCountryCon);
+
+    /**
+     * 根据市控查询站点信息
+     * @param isCityCon
+     * @return
+     */
+    List<Station> findByCityCon(int isCityCon);
+
+    /**
+     * 根据区控查询站点信息
+     * @param isDomainCon
+     * @return
+     */
+    List<Station> findByDomainCon(int isDomainCon);
+
+    /**
+     * 根据区域环境查询站点信息
+     * @param area
+     * @return
+     */
+    List<Station> findByArea(int area);
+
+    /**
+     * 根据功能区查询站点信息
+     * @param domain
+     * @return
+     */
+    List<Station> findByDomain(int domain);
+
+    /**
+     * 站点名的模糊查询
+     * @param stationName
+     * @return
+     */
+    @Query(value = "select s from station s where s.STATION_NAME like '%?1%'")
+    List<Station> findByStationNameLike(String stationName);
 
 }
