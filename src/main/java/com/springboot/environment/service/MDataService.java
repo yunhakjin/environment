05d777332402b26faa2120744be83a420848fca1
yunhakjin <yunhakjin@163.com>
2018-10-17 15:13:07 +0800

    数据部分完成基本功能
    Signed-off-by: yunhakjin <yunhakjin@163.com>

--- src/main/java/com/springboot/environment/service/MDataService.java
+++ src/main/java/com/springboot/environment/service/MDataService.java
@@ -1,4 +1,13 @@
 package com.springboot.environment.service;
+import com.springboot.environment.bean.MData;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface MDataService {
+    public List<MData> getAll();
+    public Page<MData> getAllPage(int page, int size);
+    public List<MData> getMDataByData_id(String Data_id);
+    public Page<MData> getByStationAndTime(String station_id,String starttime,String endtime,int data_check,int data_status,int page,int size);
 }
+
---
+++ src/main/java/com/springboot/environment/serviceImpl/HDataServiceImp.java
@@ -0,0 +1,40 @@
+package com.springboot.environment.serviceImpl;
+
+import com.springboot.environment.bean.HData;
+import com.springboot.environment.dao.HDataDao;
+import com.springboot.environment.service.HDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HDataServiceImp implements HDataService {
+    @Autowired
+    private HDataDao hDataDao;
+
+    @Override
+    public List<HData> getAll() {
+        return hDataDao.findAll();
+    }
+
+    @Override
+    public Page<HData> getAllPage(int page, int size){
+        Pageable pageable=PageRequest.of(page,size);
+        return hDataDao.findAll(pageable);
+    }
+
+    @Override
+    public List<HData> getHDataByData_id(String Data_id) {
+        return hDataDao.getAllByData_id(Data_id);
+    }
+
+    @Override
+    public Page<HData> getByStationAndTime(String station_id, String starttime, String endtime, int data_check, int data_status, int page, int size) {
+        Pageable pageable=PageRequest.of(page,size);
+        return hDataDao.getByStationAndTime(station_id,starttime,endtime,data_check,data_status,pageable);
+    }
+}
