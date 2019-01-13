687b11408e925ca5e2cf67da625cd97e78f67da2
yaowanwei96 <1024002148@qq.com>
2019-01-13 15:38:16 +0800

    GEOJSON超标数据修改
    Signed-off-by: yaowanwei96 <1024002148@qq.com>

--- src/main/java/com/springboot/environment/service/WarningService.java
+++ src/main/java/com/springboot/environment/service/WarningService.java
@@ -4,6 +4,7 @@ import com.springboot.environment.bean.Warning;
 
 import java.text.ParseException;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -16,5 +17,5 @@ public interface WarningService {
     public String queryWarningByDomainAndTimeAndDistrictAndStation(String warning_district, int warning_domain, String start_time, String end_time,String station_id) throws ParseException;
     public List<Warning> queryNewWarning(int lastNum);
     public int getCount();
-    public String getRealWarning();
+    public Map getRealWarning();
 }
